ee91dee555bb62ebfb94e1a6c7b00b884bfcdd30
samikshya845 <samikshyad845@gmail.com>
2025-12-21 09:49:13 +0545

    Third Commit

--- app/src/main/java/com/example/giftbox/BirthdayCategoryActivity.java
+++ app/src/main/java/com/example/giftbox/BirthdayCategoryActivity.java
@@ -1,5 +1,6 @@
 package com.example.giftbox;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.ImageView;
 
@@ -16,6 +17,7 @@ public class BirthdayCategoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_birthday_category); // your XML above
 
         ImageView ivBack = findViewById(R.id.ivBack);
-        ivBack.setOnClickListener(v -> finish());   // go back to previous Activity
+        ivBack.setOnClickListener(v -> onBackPressed());
+
     }
 }
